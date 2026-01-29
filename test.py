0294daf44f69a335e0fe1b54671e3628cb6f2df6
Yadieldiazthen <yodidiazhen@gmail.com>
2026-01-29 14:15:33 -0400

    termine resta y suma

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print("hello from yadiels computer")
+num1 = float(input("first number"))
+num2 = float(input("second number"))
+x = input("addition, subtract")
+
+if x == '+':
+    answer = num1 + num2
+elif x == '-':
+    answer = num1 - num2
