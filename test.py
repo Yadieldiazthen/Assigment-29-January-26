8f1c1d1547d3ed25d1d99850f5723a7d4aab2eaa
alexmartinezu1018-glitch <alex.martinezu1018@gmail.com>
2026-01-29 15:28:20 -0300

    termine multiplicacion y division

--- test.py
+++ test.py
@@ -1,8 +1,15 @@
 num1 = float(input("first number"))
 num2 = float(input("second number"))
-x = input("addition, subtract")
+x = input("addition, subtract, multiplication, division")
 
 if x == '+':
     answer = num1 + num2
 elif x == '-':
     answer = num1 - num2
+elif x == '*':
+    answer = num1 * num2
+elif x == '/':
+    answer = num1 / num2
+
+
+print(f"{answer}")
